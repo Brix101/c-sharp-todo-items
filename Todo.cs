51fe1a2fcb7f6498df6e9c0dccd316caea49fd6b
Brixter Porras <brixterporras@gmail.com>
2023-05-29 10:59:20 +0800

    feat: added gitignore

--- Todo.cs
+++ Todo.cs
@@ -3,4 +3,9 @@ public class Todo
     public int Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
+
+    public Todo()
+    {
+        IsComplete = false; // Set IsComplete to false by default
+    }
 }
